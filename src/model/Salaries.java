ac02d3c1585f170fe149769c876bd8ce82cbe2a2
danawacomputer <loveyou_06@naver.com>
2017-04-20 17:47:54 +0900

    //20170420 csv, stram이용

---
+++ src/model/Salaries.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Salaries {
+
+    private LocalDate yearID;
+    private String teamID;
+    private String lgID;
+    private String playerID;
+    private int salary;
+
+    public Salaries() {
+    }
+
+    public Salaries(LocalDate yearID, String teamID, String lgID, String playerID, int salary) {
+        this.yearID = yearID;
+        this.teamID = teamID;
+        this.lgID = lgID;
+        this.playerID = playerID;
+        this.salary = salary;
+    }
+
+    public LocalDate getYearID() {
+        return yearID;
+    }
+
+    public void setYearID(LocalDate yearID) {
+        this.yearID = yearID;
+    }
+
+    public String getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(String teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getLgID() {
+        return lgID;
+    }
+
+    public void setLgID(String lgID) {
+        this.lgID = lgID;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/service/SalariesService.java
@@ -0,0 +1,62 @@
+package service;
+
+import com.opencsv.CSVReader;
+import model.Salaries;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SalariesService {
+
+    public static List<Salaries> makeListFromCSV(String filename) {
+        List<Salaries> list = null;
+        try {
+            CSVReader reader = new CSVReader(new FileReader(filename));
+
+            list = new ArrayList<>();
+            reader.readNext(); // skip the first line
+            String[] spl = null;
+
+            while ((spl = reader.readNext()) != null) {
+                LocalDate localdate = LocalDate.of(Integer.parseInt(spl[0]), 1, 1);
+//                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+//                LocalDate formattedString = LocalDate.parse(localdate.format(formatter));
+                list.add(new Salaries(localdate, spl[1], spl[2], spl[3], Integer.parseInt(spl[4])));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            list = new ArrayList<>();
+        }
+
+        return list;
+    }
+
+
+    //todo: 1. 1900년대 평균연봉을 구하세요(1985~1999)
+
+
+    //todo: 2. 전체 레코드의 평균연봉을 구하세요
+
+
+    //todo: 3. 최고 연봉자와 최소 연봉자를 구하세요
+
+
+    //todo: 4. NL의 최고 연봉자를 구하세요
+
+
+    //todo: 5. NYY 구단의 평균연봉을 구하세요
+
+
+
+
+}
